32134fe7fdedfd81627d41d5b58d58df289338a1
enemyare <mishasultanov4@gmail.com>
2024-04-15 23:00:46 +0500

    Добавили компоненты и добавили стилей

---
+++ frontend/src/app/next-btn/next-btn.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-next-btn',
+  standalone: true,
+  imports: [],
+  templateUrl: './next-btn.component.html',
+  styleUrl: './next-btn.component.css'
+})
+export class NextBtnComponent {
+
+}
---
+++ frontend/src/app/sort-bar/sort-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sort-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './sort-bar.component.html',
+  styleUrl: './sort-bar.component.css'
+})
+export class SortBarComponent {
+
+}
