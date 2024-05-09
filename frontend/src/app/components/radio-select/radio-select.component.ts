8455499bd3e903c880318c3febf43464f93df8be
Nordwi <amosh.work@yandex.ru>
2024-05-09 13:35:59 +0500

    Разбил профиль на компоненты

---
+++ frontend/src/app/components/radio-select/radio-select.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-radio-select',
+  standalone: true,
+  imports: [],
+  templateUrl: './radio-select.component.html',
+  styleUrl: './radio-select.component.css'
+})
+export class RadioSelectComponent {
+
+}
