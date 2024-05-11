65ffaf0ae9630d769335ab9246cdd5b1b313459c
owbfk <artem.volchihin@mail.ru>
2024-05-11 23:42:35 +0500

    Фикс фильтра 2

--- frontend/src/app/filter.pipe.ts
+++ frontend/src/app/filter.pipe.ts
@@ -10,10 +10,10 @@ export class FilterPipe implements PipeTransform {
 
   transform(cards: Post[], filters: FilterRent): Post[] {
     return cards.filter(card => {
-      return (filters.square == 0 || filters.square == null &&
-        filters.inhabitantsCount == 0 || filters.inhabitantsCount == null &&
-        filters.maxPrice == 0 || filters.maxPrice == null) ||
-      (card.square== filters.square ||  card.inhabitantsCount == filters.inhabitantsCount || (filters.minPrice<card.price && card.price<filters.maxPrice))
+      return ((filters.square == 0 || filters.square == null) &&
+        (filters.inhabitantsCount == 0 || filters.inhabitantsCount == null) &&
+        (filters.maxPrice == 0 || filters.maxPrice == null)) ||
+      (card.square== filters.square ||  card.inhabitantsCount == filters.inhabitantsCount || (filters.minPrice<=card.price && card.price<=filters.maxPrice))
     });
   }
 }
