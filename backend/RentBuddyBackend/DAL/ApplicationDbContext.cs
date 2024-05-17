3b77b8672244ef3d14626918eb36440ab13f67e3
Soranoshie <roman.elzzza@gmail.com>
2024-05-17 14:47:24 +0500

    работает

--- backend/RentBuddyBackend/DAL/ApplicationDbContext.cs
+++ backend/RentBuddyBackend/DAL/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ public class ApplicationDbContext : DbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, Config config) : base(options)
     {
         this.config = config;
+        Database.EnsureDeleted();
         Database.EnsureCreated();
     }
 
