f09dea18e4202c4bace443bef0076fcf3b1942c3
Petr <piotr.makarov@gmail.com>
2024-06-05 19:57:09 +0500

    Added base url

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,13 +2,20 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async'
 import { httpInterceptor } from './http.interceptor';
 import { GlobalErrorHandlerService } from './services/global-error-handler.service';
+import { APIInterceptor } from './base-url.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), GlobalErrorHandlerService, HttpClientModule,importProvidersFrom(HttpClientModule) , provideAnimationsAsync(),
-    provideHttpClient(withInterceptors([httpInterceptor]))
+  providers: [
+    provideRouter(routes), provideClientHydration(), GlobalErrorHandlerService, HttpClientModule,importProvidersFrom(HttpClientModule) , provideAnimationsAsync(),
+    provideHttpClient(withInterceptors([httpInterceptor])),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: APIInterceptor,
+      multi: true,
+    },
   ]
 };
