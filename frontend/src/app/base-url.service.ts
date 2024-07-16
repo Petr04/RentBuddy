64fdffd0da567e02ce2c29a0591e9c9516215acf
Petr <piotr.makarov@gmail.com>
2024-07-16 14:05:30 +0500

    Added environment to frontend

--- frontend/src/app/base-url.service.ts
+++ frontend/src/app/base-url.service.ts
@@ -1,12 +1,13 @@
 import {Injectable} from '@angular/core';
 import {HttpEvent, HttpInterceptor, HttpHandler, HttpRequest} from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
+import { environment } from '../environments/environment';
 
 @Injectable()
 export class APIInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-    const apiReq = req.clone({ url: `http://localhost:5000/${req.url}` });
+    const apiReq = req.clone({ url: `${environment.apiUrl}/${req.url}` });
     
     return next.handle(apiReq);
   }
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  // apiUrl: 'http://158.160.51.212:5000',
+  // apiUrl: 'http://localhost:5000',
+  apiUrl: 'http://localhost:5096',
+};
